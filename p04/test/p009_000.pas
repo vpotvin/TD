497a8005f68d85716299c1303542e3ef5cf321c1
vpotvin <vdpotvin@gmail.com>
2015-02-24 22:15:11 -0600

    added test cases

---
+++ p04/test/p009_000.pas
@@ -0,0 +1,14 @@
+{------------------------------------------------------------------------------------}
+{program p009_000 exercises production #009                                          }
+{identifier_list -> identifier_list , ID                                             }
+{------------------------------------------------------------------------------------}
+{Author: Thomas R. Turner                                                            }
+{E-Mail: trturner@uco.edu                                                            }
+{Date: January, 2012                                                                 }
+{------------------------------------------------------------------------------------}
+{Copyright January, 2012 by Thomas R. Turner                                         }
+{Do not reproduce without permission from Thomas R. Turner.                          }
+{------------------------------------------------------------------------------------}
+program p009_000(input,output);
+begin{p009_000}
+end{p009_000}.
